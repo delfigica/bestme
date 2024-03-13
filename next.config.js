0b9d5f4cbdc438c527b052a9eee327d93ff35c98
Delfina Cañas <79326402+delfigica@users.noreply.github.com>
2024-03-13 10:34:40 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
+  output: "export", 
+  basePath: "/bestme",
+  reactStrictMode: true,
 }
  
 module.exports = nextConfig;
